f7857c1de45a391eb48f761fa01f58463089108f
obaydullaa <cr7obaydul@gmail.com>
2022-07-25 11:59:30 +0600

    Add Destructuring , Promise, async await and Class

--- Day_3_(23-July-2022)/js_for_React_start.js
+++ Day_3_(23-July-2022)/js_for_React_start.js
@@ -6,22 +6,25 @@
  * 2. Function declaration , arrow function and single line return
  * 3. Template string/literal
  * 4. Conditional and logical AND(&&)
- * 5. Array helper method (map, fileter, reduce)
- * 6. Destruring , rest, spread
- * 7. Promise, asunc await 
+ * 5. Array helper method (map, filter, find)
+ * 6. Destructuring , rest, spread
+ * 7. Promise, async await 
+ * 8. Class
  * 
  */
 
-// * 1. var, const let
-//===========================
+/**
+ * * 1. var, const let
+ */
 
-// var a = 7   // Dont use(to avoid unexpected error, bug)
+// var a = 7   // Don't use(to avoid unexpected error, bug)
 // const b = 8 // We always use const
-// let c = 9   // Some times use let ( basicaly  When decler or track value we use let ) 
+// let c = 9   // Some times use let ( basically  When dealer or track value we use let ) 
 
 
-//* 2. Function declaration , arrow function and single line return
-//======================================================================
+/**
+ * 2. Function declaration , arrow function and single line return
+ */
 
 // Normal function 
 // Function statement
@@ -34,7 +37,7 @@
 //     return num1 + num2;
 // }
 
-// Arro funcion
+// Arrow function
 // const sum =  (num1, num2) => {
 //     return num1 + num2;
 // }
@@ -47,23 +50,25 @@
 // console.log(multiply(10))
 
 
-// * 3. Template string/literal
-//===================================
+/**
+ * 3. Template string/literal
+ */
 
 // const firstName = 'obaydul';
 // const lastName = 'islam';
 
 // const fullName = firstName + ' ' + lastName
-// Javasript ecpression can be written inside in curly braces
-// statement - comane (;) ecpression - value or we write variable right side
+// Javascript expression can be written inside in curly braces
+// statement - command (;) expression - value or we write variable right side
 
 // const fullName = `${firstName.toUpperCase()}  ${lastName.toUpperCase()}`
 
 // console.log(fullName)
 
 
-//* 4. Conditional and logical AND(&&)
-// =======================================
+/**
+ * 4. Conditional and logical AND(&&)
+ */
 
 //if-else statement
 // const age = 19;
@@ -73,9 +78,9 @@
 
 //logical && operator
 
-// const a = age >= 15 && 'You are adult enogh'
+// const a = age >= 15 && 'You are adult'
 
-// const b = age >= 20 || 'You are adult enogh'
+// const b = age >= 20 || 'You are adult'
 
 // console.log(a);
 
@@ -83,12 +88,15 @@
 
 
 
-//* 5. Array helper method (map, fileter, reduce)
-//====================================================
+/**
+ * 5. Array helper method (map, filter, reduce)
+ */
 
-const nums = [1, 2, 3, 4, 5]
+// map
 
-function double(nums) {
+// const nums = [1, 2, 3, 4, 5]
+
+// function double(nums) {
 //     const arr = [];
     // Imperative coding
     // for(let i = 0; i < nums.length; i++) {
@@ -101,9 +109,213 @@ function double(nums) {
     // }
     // return arr;
 
-    return nums.map(num => {
-        return num * 2
-    })
+//     return nums.map(num => {
+//         return num * 2
+//     })
+// }
+
+// console.log(double(nums))
+
+
+// find ( find return immediate result not multiple result)
+// function findElm (recArr, searchelm) {
+//     return recArr.find((elm) => {
+//         // condition
+//         return elm > searchelm
+//     })
+// }
+// const nums = [1, 2, 3, 4, 5]
+
+// console.log(findElm(nums, 4));
+
+// filter ( filter return all result not immediate result)
+// function filterElm (recArr, searchelm) {
+//     return recArr.filter((elm) => {
+//         // condition
+//         return elm > searchelm
+//     })
+// }
+// const nums = [1, 2, 3, 4, 5]
+
+// console.log(filterElm(nums, 2));
+
+
+/**
+ * 6. Destructuring , rest, spread
+ */
+
+// Note: spread and rest operator modified original array.
+
+// Destructuring 
+// const person = {
+//     firstName: 'Obaydul',
+//     lastName: 'Islam',
+//     age: 25,
+// }
+
+
+// normally access
+// console.log(person.fistName + ' ' + person.lastName)
+
+// const {firstName, age, lastName} = person
+
+// console.log( age, lastName, firstName )
+
+// rest operator
+// const {firstName, ...restValues} = person
+// console.log(firstName)
+// console.log(restValues)
+
+
+// spared operator
+// const newPersonObj = {
+//     ...person,
+//     profession: 'Programmer',
+// }
+
+// console.log(newPersonObj)
+
+// const arr = [1, 2, 3, 4]
+// const [num1, num2, ...restArr] = arr
+// console.log(restArr)
+
+// const newArr = [...arr, 5]
+// console.log(newArr)
+
+// function viewPerson({firstName, lastName, age}){
+//     console.log(firstName, lastName, age)
+
+// }
+
+// const person = {
+//     firstName: 'Obaydul',
+//     lastName: 'Islam',
+//     age: 25,
+// }
+
+// viewPerson(person)
+
+/**
+ *  * 7. Promise, async await 
+ * ================================
+ * 
+ * Data Process oR Deal 3 ways
+ *  1. Callback, 2, Promise. 3. Async
+ * 
+ */
+
+// Pending
+// Resolved
+// Rejected
+
+// Promise way
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.log(err))
+ 
+//   // Async await 
+//   async function run() {
+//    try{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     const data = await res.json();
+//     console.log(data)
+//    }catch(err) {
+//     console.log(err)
+//    }
+
+//   }
+//  run();
+
+// Follow this code (Miraz Vai)
+///==================================
+// (() => {
+  /**
+   * Promise with Error handling
+   * 
+   */
+
+    // fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //   // Pending State
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //     throw new Error("DATA Loaded failed while Fetching ...");
+    //   })
+    //   // Resolved State
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   // Rejected State
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+
+  /**
+   * async await with Error handling
+   */
+
+    // async function run() {
+    //   try {
+    //     let response = await fetch(
+    //       "https://jsonplaceholder.typicode.com/todos/1"
+    //     );
+    //     if (!response.ok) {
+    //       throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
+    //     let data = await response.json();
+    //     console.log(data);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
+    // run();
+// })();
+
+
+/**
+ * 8. Class
+ */
+
+class Person {
+  //filed/property
+  // name = 'obaydul';
+  // age = 24;
+
+  constructor(mName, mAge) {
+    console.log(mName, mAge)
+    this.name = mName
+    this.age = mAge
+  }
+
+  //method
+  showInfo(){
+    return this.name + '-' + this.age
+  }
 }
 
-console.log(double(nums))
+// const person = new Person('islam', 27)
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+//   console.log(person.showInfo()
+
+class Student extends Person{
+  constructor(name, age, course) {
+    super(name, age)
+    this.course = course;
+  }
+
+  showProfInfo() {
+    
+    return super.showInfo() + ' - ' + this.course 
+  }
+}
+
+const student = new Student('Razue', 26, 'React Student')
+console.log(student)
+console.log(student.showInfo())
+
+console.log(student)
