df3bcb1fcb9a2b2ca3e70c44af957c5bdbb5d2d1
obaydullaa <cr7obaydul@gmail.com>
2022-08-03 09:25:28 +0600

    Add Day_13 folder Added.

---
+++ Day_13_(02-Aug-2022) (lifecycle)/react-lifecycle-method.js
@@ -0,0 +1,14 @@
+/**
+ * Topic: Lifecycle methods , Hooks
+ * =====================================
+ * 
+ *  1. Lifecycle methods , Hooks
+ * 2. Apply in Quiz Application on netlify
+ * 
+ * Lifecycle -> baby     -> Death 
+ * Component            -> class based component (Lifecycle method), functional component (lifecycle hooks)
+ * formation, UI view    -> Mounting
+ * Update on UI          -> update
+ * Remove from screen    -> unmounting
+ * 
+ */
