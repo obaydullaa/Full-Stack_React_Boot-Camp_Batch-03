6038f584edd77ca82a46f6bb0e9a3b036761cdc3
obaydullaa <cr7obaydul@gmail.com>
2022-08-08 09:29:00 +0600

    add issue-tracker project, botrstrap and note file

---
+++ Day_18_(07-Aug-2022)/9_exploring_React_Bootstram.js
@@ -0,0 +1,27 @@
+/**
+ * Topic: Exploring React Bootstrap
+ * 
+ *  ** vite **
+ * 1. React Bootstrap
+ * 2. React From 
+ *      - controlled component
+ *      - uncontrolled component
+ * 3. HomeWork 
+ * 
+ * command ->
+ * 1. yarn create vite
+ * 2. yarn create vite issue-tracker --template react (Problem) / npm create vite@latest issue-tracker -- --template react
+ * 3. yarn install
+ * 4. file extension name must be .jsx
+ * 
+ * React bootstrap / reactStrap
+ *=====================================
+ *
+ * 1.yarn add react-bootstrap bootstrap
+ * 2. react bootstrap css file import in main.jsx Or App.jsx -> import 'bootstrap/dist/css/bootstrap.min.css';
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ Day_18_(07-Aug-2022)/issue-tracker/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Hello from react vite</h2>
+    </div>
+  )
+}
+
+export default App
