de13aa9aee84d6013e0e69771fe4d23020c2a08e
obaydullaa <cr7obaydul@gmail.com>
2022-08-09 11:59:20 +0600

    Add Dev-Manager Project

---
+++ Day_19_(08-Aug-2022) (React-Bootstrap -by-projects)/Hospital-Health/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Hello from react vite</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ Day_20_(09-Aug-2022) (Dev-manager)/Dev-Manager/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Hello from react vite</h2>
+    </div>
+  )
+}
+
+export default App
