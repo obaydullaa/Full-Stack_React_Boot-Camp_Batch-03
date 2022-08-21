4c2f57cef7ed132939acd628f21fa9e4b8b7dabd
obaydullaa <cr7obaydul@gmail.com>
2022-08-21 21:28:28 +0600

    Strapri server install.

---
+++ Day_32_(21-Aug-2022) ( Strapi )/strapi_Note.js
@@ -0,0 +1,11 @@
+/**
+ * Date: 21-08-22
+ * Topic: Strapi
+ * 
+ * mail: mdobaydulla17@gmail.com
+ * Pass: Obaydul17
+ */
+
+Strapi provied row Data provied.
+
+1. Install Strapi: yarn create strapi-app dev-manager-backend --quickstart
