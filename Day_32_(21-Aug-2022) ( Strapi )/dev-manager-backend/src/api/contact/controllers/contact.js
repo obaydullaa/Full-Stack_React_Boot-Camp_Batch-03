37916bb4a038ac665a6d6eba90320e136fff77d7
obaydullaa <cr7obaydul@gmail.com>
2022-08-30 16:31:09 +0600

    Adding Cant not delete another author.

--- Day_32_(21-Aug-2022) ( Strapi )/dev-manager-backend/src/api/contact/controllers/contact.js
+++ Day_32_(21-Aug-2022) ( Strapi )/dev-manager-backend/src/api/contact/controllers/contact.js
@@ -8,9 +8,25 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::contact.contact', ({strapi}) => ({
     async delete(ctx) {
-        console.log(ctx.state.user);
-        console.log(ctx.params.id);
-        console.log(ctx.request.body);
+        // console.log(ctx.state.user);
+        // console.log(ctx.params.id);
+        // console.log(ctx.request.body);
+        // console.log(id, typeof id);
+
+        const {id} = ctx.params;
+        const {id: authId} = ctx.state.user
+        const contact = await strapi.entityService.findOne(
+            "api::contact.contact", 
+            +id,
+            {
+            populate: "author"
+        }
+        );
+
+        if(!contact) return ctx.notFound('contact is not found to be deleted')
+        if(contact.author.id !== authId) 
+            return ctx.unauthorized('Your are not the owner of the contact')
+
         const response = super.delete(ctx);
         return response;
     },
