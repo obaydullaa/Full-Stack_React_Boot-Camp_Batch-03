e8244905104f22fd9c8f9b375d7c86655ed01a56
obaydullaa <cr7obaydul@gmail.com>
2024-09-15 22:07:02 +0600

    props received and children props

--- React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/rnext/src/App.jsx
+++ React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/rnext/src/App.jsx
@@ -1,26 +1,14 @@
-function App() {
-    return (
-        // <div>
-        //     <Gallery />
-        //     <Profile />
-        //     <Gallery />
-        //     <Card />
-        //     <Card />
-        // </div>
+import Avatar from "./components/Avatar";
+import Profile from "./components/Profile";
 
-        <>
-            <img
-                src="https://i.imgur.com/yXOvdOSs.jpg"
-                alt="Hedy Lamarr"
-                className="photo"
-            />
-            <ul>
-                <li className="hellow">Invent new traffic lights</li>
-                <li>Rehearse a movie scene</li>
-                <li>Improve the spectrum technology</li>
-            </ul>
-        </>
+export default function app() {
+    return (
+        <div>
+            <div>
+                <Profile>
+                    <Avatar />
+                </Profile>
+            </div>
+        </div>
     );
 }
-
-export default App;
---
+++ React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/rnext/src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Profile 
+                </h1>
+            </div>
+        </div>
+    );
+}
