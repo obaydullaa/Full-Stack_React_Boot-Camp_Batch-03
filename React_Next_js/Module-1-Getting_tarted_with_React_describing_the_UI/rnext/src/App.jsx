0cd8844ae4b1d54dad53e6e65d45a2b8292eedaa
obaydullaa <cr7obaydul@gmail.com>
2024-09-16 11:16:54 +0600

    rendering lis use filter and map

--- React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/rnext/src/App.jsx
+++ React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/rnext/src/App.jsx
@@ -1,11 +1,58 @@
-import PackingList from "./components/PakingList";
+// const people = [
+//     "Creola Katherine Johnson: mathematician",
+//     "Mario José Molina-Pasquel Henríquez: chemist",
+//     "Mohammad Abdus Salam: physicist",
+//     "Percy Lavon Julian: chemist",
+//     "Subrahmanyan Chandrasekhar: astrophysicist",
+// ];
 
+// let itemList = people.map((person) => <li>{person}</li>);
 
+// export default function app() {
+//     return (
+//         <div>
+//             <ul>{itemList}</ul>
+//         </div>
+//     );
+// }
+
+const people = [
+    {
+        id: 0,
+        name: "Creola Katherine Johnson",
+        profession: "mathematician",
+    },
+    {
+        id: 1,
+        name: "Mario José Molina-Pasquel Henríquez",
+        profession: "chemist",
+    },
+    {
+        id: 2,
+        name: "Mohammad Abdus Salam",
+        profession: "physicist",
+    },
+    {
+        id: 3,
+        name: "Percy Lavon Julian",
+        profession: "chemist",
+    },
+    {
+        id: 4,
+        name: "Subrahmanyan Chandrasekhar",
+        profession: "astrophysicist",
+    },
+];
 
 export default function app() {
+    let chemists = people.filter((person) => person.profession === "chemist");
+    let itemList = chemists.map((chemist) => <li key={chemist.id}>{chemist.name}</li>);
+
     return (
         <div>
-            <PackingList />
+            <ul>
+                <li>{itemList}</li>
+            </ul>
         </div>
     );
 }
