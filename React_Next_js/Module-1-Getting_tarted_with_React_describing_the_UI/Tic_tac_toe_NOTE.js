3e38f247c38cfeb4e4e95785c091d0ab86762aaf
obaydullaa <cr7obaydul@gmail.com>
2024-09-17 11:15:44 +0600

    winner declar done.

--- React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/Tic_tac_toe_NOTE.js
+++ React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/Tic_tac_toe_NOTE.js
@@ -62,6 +62,7 @@ tailwind.config.js:
    */
 
 //   Steps: 02
+//-----------------------------------------------------------------------------------------------------------
 
 	import { useState } from "react";
 
@@ -106,6 +107,7 @@ tailwind.config.js:
 	
 
 	// step: 03
+	//-------------------------------------------------------------------------------
 
 	
 function Square({ value, squareClick }) {
@@ -146,3 +148,105 @@ export default function Board() {
         </>
     );
 }
+
+
+// step: 03 (winner dealer done)
+//-------------------------------------------------------------
+
+/**
+  Game
+    -> Board
+      -> Square
+    -> History
+ */
+
+	import { useState } from "react";
+
+	function Square({ value, squareClick }) {
+		return (
+			<button
+				className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg  cursor-pointer"
+				onClick={squareClick}
+			>
+				{value}
+			</button>
+		);
+	}
+	
+	export default function Board() {
+		const [squares, setSquares] = useState(Array(9).fill(null));
+		const [xIsNext, SetXIsNext] = useState(true);
+	
+		const winner = calculateWinner(squares);
+	
+		let status;
+	
+		if (winner) {
+			status = `Winner: ${winner}`;
+		} else {
+			status = "Next Player:" + (xIsNext ? "X" : "O");
+		}
+	
+		function handClick(i) {
+	
+		  if (squares[i] || calculateWinner(squares)) {
+			  return;
+		  }
+	
+			const nextSquares = squares.slice();
+			if (xIsNext) {
+				nextSquares[i] = "X";
+			} else {
+				nextSquares[i] = "O";
+			}
+	
+			setSquares(nextSquares);
+			SetXIsNext(!xIsNext);
+		}
+	
+		return (
+			<>
+				<div>{status}</div>
+				<div className="flex">
+					<Square value={squares[0]} squareClick={() => handClick(0)} />
+					<Square value={squares[1]} squareClick={() => handClick(1)} />
+					<Square value={squares[2]} squareClick={() => handClick(2)} />
+				</div>
+				<div className="flex">
+					<Square value={squares[3]} squareClick={() => handClick(3)} />
+					<Square value={squares[4]} squareClick={() => handClick(4)} />
+					<Square value={squares[5]} squareClick={() => handClick(5)} />
+				</div>
+				<div className="flex">
+					<Square value={squares[6]} squareClick={() => handClick(6)} />
+					<Square value={squares[7]} squareClick={() => handClick(7)} />
+					<Square value={squares[8]} squareClick={() => handClick(8)} />
+				</div>
+			</>
+		);
+	}
+	
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a];
+			}
+		}
+		return null;
+	}
+	
--- React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/tic-tac-toe/src/App.jsx
+++ React_Next_js/Module-1-Getting_tarted_with_React_describing_the_UI/tic-tac-toe/src/App.jsx
@@ -20,28 +20,77 @@ function Square({ value, squareClick }) {
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, SetXIsNext] = useState(true);
 
-    function handClick() {
-      console.log('clicked')
+    const winner = calculateWinner(squares);
+
+    let status;
+
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = "Next Player:" + (xIsNext ? "X" : "O");
+    }
+
+    function handClick(i) {
+
+      if (squares[i] || calculateWinner(squares)) {
+          return;
+      }
+
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+
+        setSquares(nextSquares);
+        SetXIsNext(!xIsNext);
     }
 
     return (
         <>
+            <div>{status}</div>
             <div className="flex">
-                <Square value={squares[0]}  squareClick={handClick} />
-                <Square value={squares[1]}  squareClick={handClick} />
-                <Square value={squares[2]}  squareClick={handClick} />
+                <Square value={squares[0]} squareClick={() => handClick(0)} />
+                <Square value={squares[1]} squareClick={() => handClick(1)} />
+                <Square value={squares[2]} squareClick={() => handClick(2)} />
             </div>
             <div className="flex">
-                <Square value={squares[3]}  squareClick={handClick} />
-                <Square value={squares[4]}  squareClick={handClick} />
-                <Square value={squares[5]}  squareClick={handClick} />
+                <Square value={squares[3]} squareClick={() => handClick(3)} />
+                <Square value={squares[4]} squareClick={() => handClick(4)} />
+                <Square value={squares[5]} squareClick={() => handClick(5)} />
             </div>
             <div className="flex">
-                <Square value={squares[6]}  squareClick={handClick} />
-                <Square value={squares[7]}  squareClick={handClick} />
-                <Square value={squares[8]}  squareClick={handClick} />
+                <Square value={squares[6]} squareClick={() => handClick(6)} />
+                <Square value={squares[7]} squareClick={() => handClick(7)} />
+                <Square value={squares[8]} squareClick={() => handClick(8)} />
             </div>
         </>
     );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 }
