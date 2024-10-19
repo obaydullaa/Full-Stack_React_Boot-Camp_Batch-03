776445299f54501e85278047a152ffd1863b02a4
obaydullaa <cr7obaydul@gmail.com>
2024-10-19 22:40:38 +0600

    event handaling and naming event handaler and .

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/rnext/src/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/rnext/src/App.jsx
@@ -1,58 +1,94 @@
-// const people = [
-//     "Creola Katherine Johnson: mathematician",
-//     "Mario José Molina-Pasquel Henríquez: chemist",
-//     "Mohammad Abdus Salam: physicist",
-//     "Percy Lavon Julian: chemist",
-//     "Subrahmanyan Chandrasekhar: astrophysicist",
-// ];
+/**
+    Responding to Events
+    React lets you add event handlers to your JSX. Event handlers are your own functions that will be triggered in response to interactions like clicking, hovering, focusing form inputs, and so on.
 
-// let itemList = people.map((person) => <li>{person}</li>);
+    You will learn
+        Different ways to write an event handler
+        How to pass event handling logic from a parent component
+        How events propagate and how to stop them
+ */
 
-// export default function app() {
+// export default function App() {
+//     function handleClick() {
+//         console.log('You clicked me!');
+//       }
+
+//     return (
+//       <button onClick={handleClick}>
+//         I dont do anything
+//       </button>
+//     );
+//   }
+
+/**
+    Reading props in event handlers 
+    =======================================================================
+ */
+
+// function AlertButton({ message, children }) {
+//     return (
+//       <button onClick={() => alert(message)}>
+//         {children}
+//       </button>
+//     );
+//   }
+
+//   export default function Toolbar() {
+//     return (
+//       <div>
+//         <AlertButton message="Playing!">
+//           Play Movie
+//         </AlertButton>
+//         <AlertButton message="Uploading!">
+//           Upload Image
+//         </AlertButton>
+//       </div>
+//     );
+//   }
+
+// function Button({ onClick, children }) {
+//     return <button onClick={onClick}>{children}</button>;
+// }
+
+// function PlayButton({ movieName }) {
+//     function handlePlayClick() {
+//         alert(`Playing ${movieName}!`);
+//     }
+
+//     return <Button onClick={handlePlayClick}>Play {movieName}</Button>;
+// }
+
+// function UploadButton() {
+//     return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+// }
+
+// export default function Toolbar() {
 //     return (
 //         <div>
-//             <ul>{itemList}</ul>
+//             <PlayButton movieName="Kiki's Delivery Service" />
+//             <UploadButton />
 //         </div>
 //     );
 // }
 
-const people = [
-    {
-        id: 0,
-        name: "Creola Katherine Johnson",
-        profession: "mathematician",
-    },
-    {
-        id: 1,
-        name: "Mario José Molina-Pasquel Henríquez",
-        profession: "chemist",
-    },
-    {
-        id: 2,
-        name: "Mohammad Abdus Salam",
-        profession: "physicist",
-    },
-    {
-        id: 3,
-        name: "Percy Lavon Julian",
-        profession: "chemist",
-    },
-    {
-        id: 4,
-        name: "Subrahmanyan Chandrasekhar",
-        profession: "astrophysicist",
-    },
-];
+export default function App() {
+    return (
+        <Toolbar
+            onPlayMovie={() => alert("Playing!")}
+            onUploadImage={() => alert("Uploading!")}
+        />
+    );
+}
 
-export default function app() {
-    let chemists = people.filter((person) => person.profession === "chemist");
-    let itemList = chemists.map((chemist) => <li key={chemist.id}>{chemist.name}</li>);
-
+function Toolbar({ onPlayMovie, onUploadImage }) {
     return (
         <div>
-            <ul>
-                <li>{itemList}</li>
-            </ul>
+            <Button onSmash={onPlayMovie}>Play Movie </Button>
+            <Button onSmash={onUploadImage}>Upload Image</Button>
         </div>
     );
+}
+
+function Button({ onSmash, children }) {
+    return <button onClick={onSmash}>{children}</button>;
 }
