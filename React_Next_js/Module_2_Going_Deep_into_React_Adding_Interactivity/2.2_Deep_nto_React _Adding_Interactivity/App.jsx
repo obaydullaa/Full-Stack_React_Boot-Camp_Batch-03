ca04e79ca06116930b8727fbf251153662cb9599
obaydullaa <cr7obaydul@gmail.com>
2024-10-21 09:46:36 +0600

    Stopping propagation

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.2_Deep_nto_React _Adding_Interactivity/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.2_Deep_nto_React _Adding_Interactivity/App.jsx
@@ -1,18 +1,51 @@
 // Event propagation
 //==============================================
 
-export default function Toolbar() {
-    return (
-        <div
-            className="Toolbar"
-            onClick={() => {
-                console.log("You clicked on the toolbar!");
-            }}
-        >
-            <button onClick={() => console.log("Playing!")}>Play Movie</button>
-            <button onClick={() => console.log("Uploading!")}>
-                Upload Image
-            </button>
-        </div>
-    );
-}
+// export default function Toolbar() {
+//     return (
+//         <div
+//             className="Toolbar"
+//             onClick={() => {
+//                 console.log("You clicked on the toolbar!");
+//             }}
+//         >
+//             <button onClick={() => console.log("Playing!")}>Play Movie</button>
+//             <button onClick={() => console.log("Uploading!")}>
+//                 Upload Image
+//             </button>
+//         </div>
+//     );
+// }
+
+
+// Stopping propagation 
+//==============================================
+
+
+// function Button({ onSmash, children }) {
+//     return (
+//       <button onClick={(e) => {
+//         e.stopPropagation();
+//         onSmash();
+//       }}
+//       >
+//         {children}
+//       </button>
+//     );
+//   }
+  
+//   export default function Toolbar() {
+//     return (
+//       <div className="Toolbar" onClick={() => {
+//         console.log('You clicked on the toolbar!');
+//       }}>
+//         <Button onSmash={() => console.log('Playing!')}>
+//           Play Movie
+//         </Button>
+//         <Button onSmash={() => console.log('Uploading!')}>
+//           Upload Image
+//         </Button>
+//       </div>
+//     );
+//   }
+  
--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/rnext/src/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/rnext/src/App.jsx
@@ -111,3 +111,34 @@
 // }
 
 
+// Stopping propagation 
+//==============================================
+
+
+function Button({ onSmash, children }) {
+    return (
+      <button onClick={(e) => {
+        e.stopPropagation();
+        onSmash();
+      }}
+      >
+        {children}
+      </button>
+    );
+  }
+  
+  export default function Toolbar() {
+    return (
+      <div className="Toolbar" onClick={() => {
+        console.log('You clicked on the toolbar!');
+      }}>
+        <Button onSmash={() => console.log('Playing!')}>
+          Play Movie
+        </Button>
+        <Button onSmash={() => console.log('Uploading!')}>
+          Upload Image
+        </Button>
+      </div>
+    );
+  }
+  
