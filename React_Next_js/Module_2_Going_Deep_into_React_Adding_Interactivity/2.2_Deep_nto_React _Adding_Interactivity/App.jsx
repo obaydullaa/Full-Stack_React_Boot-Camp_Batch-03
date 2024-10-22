4efcb5d4f9fc1d4bd4af69b7887819f510b98bd7
obaydullaa <cr7obaydul@gmail.com>
2024-10-22 23:32:09 +0600

    add note handler react.

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.2_Deep_nto_React _Adding_Interactivity/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.2_Deep_nto_React _Adding_Interactivity/App.jsx
@@ -85,3 +85,20 @@
 // }
 
   
+
+
+
+
+
+
+
+// Recap
+// You can handle events by passing a function as a prop to an element like <button>.
+// Event handlers must be passed, not called! onClick={handleClick}, not onClick={handleClick()}.
+// You can define an event handler function separately or inline.
+// Event handlers are defined inside a component, so they can access props.
+// You can declare an event handler in a parent and pass it as a prop to a child.
+// You can define your own event handler props with application-specific names.
+// Events propagate upwards. Call e.stopPropagation() on the first argument to prevent that.
+// Events may have unwanted default browser behavior. Call e.preventDefault() to prevent that.
+// Explicitly calling an event handler prop from a child handler is a good alternative to propagation.
