9db821099f0a74733c04d9dc70327743acca6056
obaydullaa <cr7obaydul@gmail.com>
2024-10-26 15:11:17 +0600

    Render and Commit.

---
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.4_How_state_works_in_React _A_deep_dive/App.jsx
@@ -0,0 +1,44 @@
+// State: A Component's Memory
+//=================================================
+
+// import { sculptureList } from './data.js';
+// import { useState } from "react";
+
+// export default function Gallery() {
+//    const [index, setIndex] = useState(0);
+
+//   function handleClick() {
+//     // index = index + 1;
+//      setIndex(index + 1);
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Next
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         by {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} of {sculptureList.length})
+//       </h3>
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//       <p>
+//         {sculpture.description}
+//       </p>
+//     </>
+//   );
+// }
+
+
+
+// State: A Component's Memory Deep Drive
+//=================================================
+
+
---
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.5_How_Rendering_works/App.jsx
@@ -0,0 +1,10 @@
+// Render and Commit
+//=================================================
+// Recap
+// Any screen update in a React app happens in three steps:
+// Trigger
+// Render
+// Commit
+// You can use Strict Mode to find mistakes in your components
+// React does not touch the DOM if the rendering result is the same as last time
+
