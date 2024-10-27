078b5dfc4ce66de6e56a093dd8127fa626cbed0f
obaydullaa <cr7obaydul@gmail.com>
2024-10-27 07:37:37 +0600

    added react updating arrays in state.

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.9_Updating_Arrays_in_a_State/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/2.9_Updating_Arrays_in_a_State/App.jsx
@@ -1,4 +1,8 @@
 // Updating Arrays in State
 //=================================================
 // Recap
-
+// You can put arrays into state, but you can’t change them.
+// Instead of mutating an array, create a new version of it, and update the state to it.
+// You can use the [...arr, newItem] array spread syntax to create arrays with new items.
+// You can use filter() and map() to create new arrays with filtered or transformed items.
+// You can use Immer to keep your code concise.
