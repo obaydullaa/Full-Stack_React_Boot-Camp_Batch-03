d1d07fdb39992b37c89a0a879d1b28bba6e18917
obaydullaa <cr7obaydul@gmail.com>
2024-10-29 08:38:46 +0600

    added tailwind css config and project setup.

---
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-red-700 text-4xl mt-8 font-bold">Welcome To Tasker</h2>
+    </>
+  )
+}
+
+export default App
