6d2f89b8065e1f491377300ffc41a67d7c826efb
obaydullaa <cr7obaydul@gmail.com>
2024-10-31 08:03:56 +0600

    adde footer and hero section.

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 export default function App() {
     return (
         <>
           <Header />
+          <HeroSection />
+          <Footer />
         </>
     );
 }
