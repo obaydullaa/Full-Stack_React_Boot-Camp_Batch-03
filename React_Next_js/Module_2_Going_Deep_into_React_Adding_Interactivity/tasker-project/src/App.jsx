ed17a1827dc9e46d1a10fa77c1f087d75cf3e2db
obaydullaa <cr7obaydul@gmail.com>
2024-11-01 06:25:58 +0600

    completed create all component.

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/App.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/App.jsx
@@ -1,13 +1,17 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 export default function App() {
     return (
         <>
-          <Header />
-          <HeroSection />
-          <Footer />
+            <Header />
+            <div className="flex flex-col justify-center items-center">
+                <HeroSection />
+                <TaskBoard />
+            </div>
+            <Footer />
         </>
     );
 }
---
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/task/TaskActions.jsx
@@ -0,0 +1,15 @@
+export default function TaskActions() {
+    return (
+        <div className="mb-14 items-center justify-between sm:flex">
+            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
+            <div className="flex items-center space-x-5">
+                <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+                    Add Task
+                </button>
+                <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
+                    Delete All
+                </button>
+            </div>
+        </div>
+    );
+}
