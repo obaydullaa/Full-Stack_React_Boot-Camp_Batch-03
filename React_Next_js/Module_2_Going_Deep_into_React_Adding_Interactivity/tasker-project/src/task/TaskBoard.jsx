915ffbb4c46ffbe6bd447ef1e634e3ba941b8853
obaydullaa <cr7obaydul@gmail.com>
2024-11-03 07:40:03 +0600

    added taskt board show from state.

--- React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/task/TaskBoard.jsx
+++ React_Next_js/Module_2_Going_Deep_into_React_Adding_Interactivity/tasker-project/src/task/TaskBoard.jsx
@@ -1,38 +1,31 @@
-import {useState} from "react";
+import { useState } from "react";
 
 import SearchTask from "./SearchTask";
-import TaskActions from "./TaskActions"
+import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
-
 export default function TaskBoard() {
-
     const defaultTask = {
         'id': crypto.randomUUID(),
         'title': "Learn React",
         'descriptions': "I want to learn React such than i can treat it like my salve and make it do whatever I want to do",
         'tags': ["web", "react", "js"],
-        "priority": "high",
-        'isFavorite': true
-    }
+        'priority': "Low",
+        'isFavorite': true,
+    };
 
-
-   const [task, setTask] = useState([defaultTask])
+    const [tasks, setTasks] = useState([defaultTask]);
     return (
         <section className="mb-20" id="tasks">
             <div className="container">
                 <div className="p-2 flex justify-end">
-
-                  <SearchTask />
-
+                    <SearchTask />
                 </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            
-                <TaskActions tasks={task}/>
+                    <TaskActions />
 
-                <TaskList />
-
+                    <TaskList tasks={tasks} />
                 </div>
             </div>
         </section>
